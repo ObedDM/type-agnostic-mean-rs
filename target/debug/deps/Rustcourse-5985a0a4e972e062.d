57a7ed5d16f58a2efce97114b7c0d7090a659a6e
ObedDM <pacocobot2@gmail.com>
2024-10-22 07:42:27 -0600

    removed useless code

--- target/debug/deps/Rustcourse-5985a0a4e972e062.d
+++ target/debug/deps/Rustcourse-5985a0a4e972e062.d
@@ -1,5 +1,5 @@
-C:\Users\pacoc\Desktop\myRustProjects\Rustcourse\target\debug\deps\libRustcourse-5985a0a4e972e062.rmeta: src/main.rs
+C:\Users\pacoc\Desktop\myRustProjects\type_ag_mean_rs\target\debug\deps\libRustcourse-5985a0a4e972e062.rmeta: src/main.rs
 
-C:\Users\pacoc\Desktop\myRustProjects\Rustcourse\target\debug\deps\Rustcourse-5985a0a4e972e062.d: src/main.rs
+C:\Users\pacoc\Desktop\myRustProjects\type_ag_mean_rs\target\debug\deps\Rustcourse-5985a0a4e972e062.d: src/main.rs
 
 src/main.rs:
--- target/debug/deps/Rustcourse-60904d4e5ebe5766.d
+++ target/debug/deps/Rustcourse-60904d4e5ebe5766.d
@@ -1,5 +1,5 @@
-C:\Users\pacoc\Desktop\myRustProjects\Rustcourse\target\debug\deps\libRustcourse-60904d4e5ebe5766.rmeta: src/main.rs
+C:\Users\pacoc\Desktop\myRustProjects\type_ag_mean_rs\target\debug\deps\libRustcourse-60904d4e5ebe5766.rmeta: src/main.rs
 
-C:\Users\pacoc\Desktop\myRustProjects\Rustcourse\target\debug\deps\Rustcourse-60904d4e5ebe5766.d: src/main.rs
+C:\Users\pacoc\Desktop\myRustProjects\type_ag_mean_rs\target\debug\deps\Rustcourse-60904d4e5ebe5766.d: src/main.rs
 
 src/main.rs:
